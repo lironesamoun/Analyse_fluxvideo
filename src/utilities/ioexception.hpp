9db8253a4a86caf4135f1ebf1bf339ef46e8077e
lilouch <lirone.samoun@gmail.com>
2014-11-27 16:12:17 +0100

    Rajout fichier ioexeption

---
+++ src/utilities/ioexception.hpp
@@ -0,0 +1,25 @@
+#ifndef DRONE_IOEXCEPTION_HPP
+#define DRONE_IOEXCEPTION_HPP
+#include <string>
+#include <exception>
+#include <stdexcept>
+
+namespace ssm{
+class IOException:runtime_error
+{
+private:
+    const char * m_message;
+public:
+    IOException(const char * message):runtime_error(message){
+        this->m_message=message;
+    }
+    const char* what() const throw()
+       {
+           return m_message;
+       }
+
+};
+
+
+}
+#endif // DRONE_IOEXCEPTION_HPP
